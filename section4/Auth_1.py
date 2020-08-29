84b74afeb6140db246ee3cbcd01a15dc406160b8
pavan-elisetty <57026704+pavan-elisetty@users.noreply.github.com>
2020-08-29 12:24:54 +0530

    Add files via upload

---
+++ section4/Auth_1.py
@@ -0,0 +1,76 @@
+from flask import Flask , request
+from flask_restful import Resource , Api , reqparse
+from flask_jwt import JWT , jwt_required
+from security import authenticate , identity
+
+
+app = Flask(__name__)
+app.secret_key = 'jose'
+api = Api(app)
+jwt = JWT(app , authenticate , identity) #creates new endpoint /auth
+
+items = []
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,  # making price mandatory
+                        help='This field cannot be left blank'
+                        )
+
+    data = parser.parse_args()
+    @jwt_required()
+    def get(self , name):
+        #next returns the first value returned by filter function
+        item = next(filter( lambda x: x['name'] == name , items))
+        return {'item':None} , 200 if item else 404
+
+
+#while using flask restful we dont need to do jsonify as it already does that
+
+
+
+    def post(self , name):
+
+
+        if next(filter(lambda x: x['name'] == name, items) , None) is not None:
+            return{'message':'An item with name {} already existed!'.format(name)} , 400
+
+        data = Item.parser.parse_args()
+        item = {'name':name , 'price':data['price']}
+        items.append(item)
+        return item , 201 #201 code for creating
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x:x['name']!=name,items))
+        return {'message':'item deleted'}
+        #filtering the items adding all the items except the item that needs to be deleted
+
+
+    def put(self , name):
+
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] ==name , items) , None)
+        #None return if nothing matches
+        if item is None:
+            item = {'name':name , 'price':data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items':items}
+
+api.add_resource(ItemList , '/items')
+
+api.add_resource(Item , '/item/<string:name>')
+
+app.run( port = 5000 , debug=True)
--- section4/security.py
+++ section4/security.py
@@ -1,19 +1,20 @@
-from werkzeug.security import safe_str_cmp
+from werkzeug.security import safe_str_cmp #better to use this for comparing strings
 from user import User
-
 users = [
-    User(1, 'user1', 'abcxyz'),
-    User(2, 'user2', 'abcxyz'),
+    User(1 , 'bob' , 'asdf')
 ]
 
-username_table = {u.username: u for u in users}
-userid_table = {u.id: u for u in users}
+username_mapping = {u.username : u for u in users}
 
-def authenticate(username, password):
-    user = username_table.get(username, None)
-    if user and safe_str_cmp(user.password, password):
+userid_mapping = {u.id : u for u in users}
+
+def authenticate (username , password):
+    user = username_mapping.get(username , None) #setting the default value to NOne if not found
+    if user and safe_str_cmp(user.password ,  password):
         return user
 
-def identity(payload):
+def identity(payload):  #payload is the content of the JWT token
     user_id = payload['identity']
-    return userid_table.get(user_id, None)
+    return userid_mapping.get(user_id , None)
+
+
